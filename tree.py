2acd934259f55fafc0eba7236ee106a2a306a729
wboeker <wendyb@alcraeft.com>
2019-02-05 23:24:04 -0500

    inconsistent tabs :(

--- tree.py
+++ tree.py
@@ -4,6 +4,16 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
+	def insertRight(self,newNode):
+		if self.right == None:
+			self.right = TreeNode(newNode)
+		else:
+			#insertRight(self.right, newNode)
+	def insertLeft (self,newNode):
+		if self.left == None:
+			self.left = TreeNode(newNode)
+		else:
+			insertLeft(self.left, newNode)
 
 #build Bob Jr Tree from Slides in code and print out
 #traverse tree in 3 ways and print out traversals
@@ -25,6 +35,59 @@ def ioTraverse (node):
 	ioTraverse(node.left)
 	print(node.val)
 	ioTraverse(node.right)
+
+#Preorder traversal root left right
+def preTraverse (node):
+	if(node == None):
+		return None
+	print(node.val)
+	preTraverse(node.left)
+	preTraverse(node.right)
+
+#Postorder traversal left right root
+def postTraverse (node):
+	if(node == None):
+		return None
+	postTraverse(node.left)
+	postTraverse(node.right)
+	print(node.val)
 	
-#call function ioTraverse
+#call functions 
 ioTraverse(BobJrTree)
+print()
+preTraverse(BobJrTree)
+print()
+postTraverse(BobJrTree)
+
+#build big number tree
+
+"""
+numberTree = TreeNode(25)
+insertRight(TreeNode,50)
+insertRight(TreeNode,70)
+insertRight(TreeNode,90)
+insertLeft(TreeNode,15)
+insertLeft(TreeNode,10)
+insertLeft(TreeNode,4)
+leftNum = TreeNode.left
+rightNum = TreeNode.right
+insertRight(leftNum,22)
+insertRight(leftNum,24)
+insertLeft(rightNum,35)
+insertLeft(rightNum,31)
+leftNum2 = leftNum.left
+rightNum2 = rightNum.right
+insertRight(leftNum2,12)
+insertLeft(rightNum2,66)
+leftRight = leftNum.right
+rightLeft = rightNum.left
+insertLeft(leftRight,18)
+insertRight(rightLeft,44)
+
+#repeat traversals
+ioTraverse(numberTree)
+print()
+preTraverse(numberTree)
+print()
+postTraverse(numberTree)
+"""
